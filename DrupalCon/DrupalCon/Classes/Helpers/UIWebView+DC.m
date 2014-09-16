cf445416d9e660c5e0d2f309a9ef0bd3ed55c394
Olexandr Poburynnyi <opoburynnyi@RosAFX-Stepanyaks-Mac-mini-2.local>
2014-09-16 16:06:50 +0300

    Add condition not to load null string in webview

--- DrupalCon/DrupalCon/Classes/Helpers/UIWebView+DC.m
+++ DrupalCon/DrupalCon/Classes/Helpers/UIWebView+DC.m
@@ -12,6 +12,7 @@
 @implementation UIWebView (DC)
 - (void)loadHTMLString:(NSString *)string
 {
+    string = (IsEmpty(string))? @"": string;
     NSString *html = [NSString stringWithFormat:@" <!DOCTYPE html> <html> <head><meta http-equiv=\"content-type\" content=\"text/html; charset=ISO-8859-1\"><link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\"></head><body>%@</body></html> ",
                         string];
     
@@ -24,4 +25,19 @@
     NSString *path = [[NSBundle mainBundle] bundlePath];
     return [NSURL fileURLWithPath:path];
 }
+
+static inline BOOL IsEmpty(id thing) {
+    
+    return thing == nil
+    
+    || ([thing respondsToSelector:@selector(length)]
+        
+        && [(NSData *)thing length] == 0)
+    
+    || ([thing respondsToSelector:@selector(count)]
+        
+        && [(NSArray *)thing count] == 0);
+    
+}
+
 @end
