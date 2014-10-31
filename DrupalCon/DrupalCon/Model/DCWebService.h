a58cc942115dacf1c10095439dd9c76cd298b60e
Olexandr Poburynnyi <olexandr.poburynnyi@lemberg.co.uk>
2014-10-31 19:15:45 +0200

    Add logic for fetching data from server

--- DrupalCon/DrupalCon/Model/DCWebService.h
+++ DrupalCon/DrupalCon/Model/DCWebService.h
@@ -30,7 +30,44 @@
 /**
  *  DCWebService manages http request/repsponse
  */
+typedef void (^SuccessResponseCallback)(NSHTTPURLResponse *response, id data);
+typedef void (^FailureResponseCallback)(NSHTTPURLResponse *response, id data, NSError *error);
+typedef void (^CompleteRequestCallback)(BOOL success, NSDictionary *result);
 
 @interface DCWebService : NSObject
+
+/**
+ *  Create NSURLRequest
+ *
+ *  @param uri        e.g path for resources
+ *  @param httpMethod GET, POST, PUT
+ *  @param options    header description with key and value
+ *
+ *  @return NSURLRequest
+ */
+- (NSURLRequest *)urlRequestForURI:(NSString *)uri
+                    withHTTPMethod:(NSString *)httpMethod
+                 withHeaderOptions:(NSDictionary *)options;
+
+/**
+ *  Fetches data from server asynchronously
+ *
+ *  @param urlRequest      NSURLRequest object with URL request, Http method etc.
+ *  @param successCallback call when request is success in global queue
+ *  @param failureCallback call when request is failure in global queue
+ */
+- (void)fetchDataFromURLRequest:(NSURLRequest *)urlRequest
+                      onSuccess:(SuccessResponseCallback)successCallback
+                        onError:(FailureResponseCallback)failureCallback;
+
+
+/**
+ *  Fetches data from server asynchronously
+ *
+ *  @param requests arrar with NSURLRequest objects
+ *  @param callback call in globar queue when all requests has finished
+ */
+- (void)fetchesDataFromURLRequests:(NSArray *)requests callBack:(CompleteRequestCallback)callback;
+
 
 @end
