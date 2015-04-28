3adff3fdf15fc32f884c8e2640eeb0156c6e2d76
Olexandr Poburynnyi <olexandr.poburynnyi@lemberg.co.uk>
2015-04-28 19:46:46 +0300

    Remove test get parameter from url requests [touch:413]

--- DrupalCon/DrupalCon/Classes/Services/DCWebService.m
+++ DrupalCon/DrupalCon/Classes/Services/DCWebService.m
@@ -55,7 +55,7 @@
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
     
 //    TODO: Remove testing property in getParameter
-    NSString *testUriParameter = [NSString stringWithFormat:@"%@/?testing=1", uri];
+    NSString *testUriParameter = uri;//[NSString stringWithFormat:@"%@/?testing=1", uri];
     NSURL *requestURL = [NSURL URLWithString:testUriParameter relativeToURL:[NSURL URLWithString:BASE_URL]];
     [request setURL:requestURL];
     request.cachePolicy = NSURLRequestReloadIgnoringLocalCacheData;
