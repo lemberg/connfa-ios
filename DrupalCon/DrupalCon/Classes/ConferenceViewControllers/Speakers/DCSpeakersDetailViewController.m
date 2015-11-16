e9a12234e05d26a434ca7418e406187b52b13519
Olexandr Poburynnyi <olexandr.poburynnyi@lemberg.co.uk>
2015-11-16 18:40:56 +0200

    Add MIT licences for each file

--- DrupalCon/DrupalCon/Classes/ConferenceViewControllers/Speakers/DCSpeakersDetailViewController.m
+++ DrupalCon/DrupalCon/Classes/ConferenceViewControllers/Speakers/DCSpeakersDetailViewController.m
@@ -1,28 +1,28 @@
-//
-//  The MIT License (MIT)
-//  Copyright (c) 2014 Lemberg Solutions Limited
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to
-//  deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//   The above copyright notice and this permission notice shall be included in
-//   all
-//  copies or substantial portions of the Software.
-//
-//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-//  FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE
-//  SOFTWARE.
-//
+/**********************************************************************************
+ *                                                                           
+ *  The MIT License (MIT)
+ *  Copyright (c) 2015 Lemberg Solutions Limited
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to
+ *  deal in the Software without restriction, including without limitation the 
+ *  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ *  sell copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *   The above copyright notice and this permission notice shall be included in
+ *   all  copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM,  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+ *  IN THE SOFTWARE.
+ *
+ *                                                                           
+ *****************************************************************************/
+
+
 
 #import "DCSpeakersDetailViewController.h"
 #import "NSDate+DC.h"
@@ -319,20 +319,6 @@ static NSString* eventCellId = @"SpeakerEventCellId";
       }
       self.navBarBackgroundView.alpha = alpha;
 
-      // Nav bar tint color
-      //            CGFloat red = 0.0, green = 0.0, blue = 0.0, colorAlpha =
-      //            alpha / maxAlpha;
-      //            [NAV_BAR_COLOR getRed:&red green:&green blue:&blue
-      //            alpha:nil];
-      //
-      //            self.navigationController.navigationBar.tintColor = [UIColor
-      //            colorWithRed:(red+(colorAlpha*(1-red)))
-      //                                                                                green:(green+(colorAlpha*(1-green)))
-      //                                                                                 blue:(blue+(colorAlpha*(1-blue)))
-      //                                                                                alpha:1.0];
-      //            self.currentBarColor =
-      //            self.navigationController.navigationBar.tintColor;
-      //
       self.navBarBackgroundTitleLabel.textColor = self.currentBarColor;
     }
   }
