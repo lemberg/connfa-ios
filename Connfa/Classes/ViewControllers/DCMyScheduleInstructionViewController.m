d5033248d72f2728e36b6ccf1f43ed2854303bf6
Oleh Kurnenkov <oleh.kurnenkov@lemberg.co.uk>
2017-04-12 15:02:19 +0300

    Add myscheduleinstructioncontroller

---
+++ Connfa/Classes/ViewControllers/DCMyScheduleInstructionViewController.m
@@ -0,0 +1,37 @@
+//
+//  DCMyScheduleInstructionViewController.m
+//  Connfa
+//
+//  Created by Oleh Kurnenkov on 4/12/17.
+//  Copyright © 2017 Lemberg Solution. All rights reserved.
+//
+
+#import "DCMyScheduleInstructionViewController.h"
+
+@interface DCMyScheduleInstructionViewController ()
+
+@end
+
+@implementation DCMyScheduleInstructionViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
