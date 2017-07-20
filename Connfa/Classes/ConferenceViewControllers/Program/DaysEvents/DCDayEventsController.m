e749ba711bd81454c636bbd01b6a8aaaed10eaf8
Oleh Kurnenkov <oleh.kurnenkov@lemberg.co.uk>
2017-07-20 14:42:53 +0300

    Fix incorrect placeholder in social events screen. [touch:#1792]

--- Connfa/Classes/ConferenceViewControllers/Program/DaysEvents/DCDayEventsController.m
+++ Connfa/Classes/ConferenceViewControllers/Program/DaysEvents/DCDayEventsController.m
@@ -133,6 +133,7 @@ DCDayEventSourceDelegate> {
       self.stubImage = [UIImage imageNamed:@"ic_no_social_events"];
       self.stubMessage = isFilterEnabled ? @"No Matching social events" : @"Currently there are no social events";
     }
+      break;
     case EDCEventStrategySharedSchedule: {
       self.stubImage = [UIImage imageNamed:@"ic_no_my_schedule"];
       self.stubMessage = @"Currently shared schedule is empty";
